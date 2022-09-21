bae44341a64f23662b5caa6fb904f95f8843ee20
Danchen Chen <chendc1993@hotmail.com>
2022-09-21 15:19:01 -0700

    Add detail activity

--- app/src/main/java/com/cdc/githuborganization/model/OrganizationUI.kt
+++ app/src/main/java/com/cdc/githuborganization/model/OrganizationUI.kt
@@ -1,8 +1,12 @@
 package com.cdc.githuborganization.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class OrganizationUI(
   val image: String? = null,
   val name: String,
   val description: String? = null,
   val githubUrl: String = ""
-)
+): Parcelable
